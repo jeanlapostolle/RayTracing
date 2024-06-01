bfd11da634d073d7258545215b08891ba826b5b6
Melcore <lapostolle.jean@gmail.com>
2024-06-01 09:05:12 +0200

    fix/space comment

--- camera.py
+++ camera.py
@@ -6,7 +6,7 @@ class Camera():
         self.resolution = resolution
         self.pitch = pitch
 
-#           Elle est déterminée par plusieurs paramètres :
+# Elle est déterminée par plusieurs paramètres :
 # - sa position dans l'espace (xr, yr, zr) et son orientation (rot_x, rot_y, rot_z)
 # - sa focale f = distance entre le centre de la caméra et son plan de projection (écran)
 # - sa résolution (res_x, res_y) et le pitch (pitch_x, pitch_y) des pixels = dimensions d'un pixel en unités du repère espace
